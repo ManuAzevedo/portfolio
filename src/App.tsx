946b5c17a80744e91860fe7fab1060a416283106
ManuAzevedo <manuely.neves@estudante.ifms.edu.br>
2023-08-29 10:06:51 -0400

    iniciando portfolio

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Header from './componentes/header/Header'
+import Filme from '.'
+import './App.css'
+
+function App() {
+  return (
+    <Header/>
+  )
+}
+export default App
---
+++ src/componentes/filme/Filme.tsx
@@ -0,0 +1,10 @@
+import Imagem from "./../"
+
+function Filme(){
+    return(
+        <div className="filme-content">
+            Filme
+         </div>
+    )
+}
+export default Filme
